b522b85a1cb086d4c40d15f070657989a7e3398d
briancatraguna <briancatraguna@rocketmail.com>
2021-01-24 09:29:28 +0700

    filtered in 7.0 mag

---
+++ star_filtering.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#Reading in Pandas Dataframe and Cleaning Data
+excel_catalogue = pd.read_excel('SAO.xlsx')
+tidy_catalogue = excel_catalogue.rename(columns = {'Unnamed: 0': 'Star ID', 'Unnamed: 1': 'RA', 'Unnamed: 2': 'DE', 'Unnamed: 3': "Magnitude"}, inplace=False)
+
+#Filtering Magnitude
+magnitude_filter = float(input("Input magnitude that you want to filter: "))
+bool_filtered = tidy_catalogue['Magnitude']<=3
+filtered_catalogue = tidy_catalogue[bool_filtered]
+
+#Saving to CSV File
+file_name = "Below_" + str(magnitude_filter) + "_SAO.csv"
+filtered_catalogue.to_csv(file_name)
